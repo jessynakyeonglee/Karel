8bbb8ac19edc2ac52add2a579ce117cc6543ce1a
leena <leena@ssfs.org>
2019-09-30 15:21:19 -0400

    9/30/19 E block

--- src/rectanglefun.java
+++ src/rectanglefun.java
@@ -6,6 +6,7 @@ public class rectanglefun
 {
     public static void main (String[]args)
     {
+
         Scanner rf = new Scanner(System.in);
         System.out.print("Width:");
         int width = rf.nextInt();
@@ -15,9 +16,12 @@ public class rectanglefun
         int X = rf.nextInt();
         System.out.print("Y:");
         int Y = rf.nextInt();
-        int perimeter = 2*width+2*length;
-        System.out.println("Perimeter:"+ perimeter);
 
+        Rectangle r = new Rectangle(X,Y,width,length);
+        double perimeter = r.getWidth()*2+r.getHeight()*2;
+        System.out.println("Perimeter:"+ perimeter);
+        r.setLocation(X-4,Y+2);
+        System.out.println("New location is ("+r.getX()+","+r.getY()+")");
         }
 
 
