16b991011ea3dfe1422f8b56c110256b05bdbed7
leena <leena@ssfs.org>
2019-10-03 10:04:09 -0400

    10/3/19 E block

---
+++ src/Fightsong.java
@@ -0,0 +1,37 @@
+public class Fightsong {
+    public static void goteam () {
+
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
+    }
+
+        public static void you () {
+
+            System.out.println("You're the best,");
+            System.out.println("In the West.");
+        }
+    public static void blank () {
+
+        System.out.println(" ");
+
+    }
+            public static void main (String[]args){
+
+                goteam();
+                blank();
+                goteam();
+                you();
+                goteam();
+                blank();
+                goteam();
+                you();
+                goteam();
+                blank();
+                goteam();
+
+
+
+            }
+        }
+
+
