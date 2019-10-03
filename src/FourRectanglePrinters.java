9dcd14f1a8762d8675624ab8792eb8a0a51d969e
leena <leena@ssfs.org>
2019-10-03 09:49:33 -0400

    10/3/19 E block

--- src/FourRectanglePrinters.java
+++ src/FourRectanglePrinters.java
@@ -3,6 +3,16 @@ import java.awt.*;
 public class FourRectanglePrinters {
     public static void main (String[]args){
 
-        Rectangle box = new Rectangle();
+        Rectangle box = new Rectangle(0,0,5,10);
+        System.out.println(box);
+        box.translate(5,0);
+        System.out.println(box);
+        box.translate(5,-10);
+        System.out.println(box);
+        box.translate(0,-10);
+        System.out.println(box);
+
+
+
     }
 }
