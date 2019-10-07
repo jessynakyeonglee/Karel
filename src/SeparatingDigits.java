68922ea8642f6831309c11ca6b0c2441989d52d7
leena <leena@ssfs.org>
2019-10-07 11:08:14 -0400

    10/7/19 E block

--- src/SeparatingDigits.java
+++ src/SeparatingDigits.java
@@ -1,5 +1,28 @@
 import java.util.Scanner;
 
 public class SeparatingDigits {
-    Scanner sd = new Scanner(System.in);
+    public static int quotient (int entered){
+        int number = entered/10;
+        return number;
+    }
+    public static int remainder (int entered2){
+        int number = entered2%10;
+        return number;
+    }
+    public static void main(String[]args) {
+        Scanner sd = new Scanner(System.in);
+        System.out.println("Enter a four digit number");
+        int init = sd.nextInt();
+        int first = remainder(init);
+        int firstremainder = quotient(init);
+        int second = remainder(firstremainder);
+        int secondremainder = quotient(firstremainder);
+        int third = remainder(secondremainder);
+        int thirdremainder = quotient(secondremainder);
+        int fourth = remainder(thirdremainder);
+        int fourthremainder = quotient(thirdremainder);
+        System.out.print(fourth+" "+third+" "+second+" "+first);
 
+
+    }
+}
