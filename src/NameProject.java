adb16956a98bbcab19898f821c923883ea4789da
leena <leena@ssfs.org>
2019-10-08 12:05:54 -0400

    10/8/19 E block

--- src/NameProject.java
+++ src/NameProject.java
@@ -4,16 +4,26 @@ public class NameProject {
     public static void main (String[]args){
 
         Scanner name = new Scanner(System.in);
-        System.out.print("Enter first name");
+        System.out.println("Enter first name");
         System.out.println("Enter last name");
         String first = name.nextLine();
         String last = name.nextLine();
-        int length = first.length()+last.length();
-        int subs = first.substring(0,2);
+        int flength = first.length();
+        int llength = last.length();
+        String firsts = first.substring(0,1)+last.substring(0,1);
+        int compare = first.compareTo(last);
+        String lastoffirst = first.substring(flength-1);
+        int index1 = last.indexOf(lastoffirst);
+        String lastoflast = last.substring(llength-1);
+        int index2 = first.indexOf(lastoflast);
 
 
-
-        System.out.println(subs);
+        System.out.println(first+" "+last);
+        System.out.println(flength+llength);
+        System.out.println(firsts);
+        System.out.println(compare);
+        System.out.println(index1);
+        System.out.println(index2);
 
 
     }
