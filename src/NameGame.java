37e4e8d4fed46aa28293f7a68eb38ec9f6ad36e8
leena <leena@ssfs.org>
2019-10-10 14:45:14 -0400

    10/10/19 E block

--- src/NameGame.java
+++ src/NameGame.java
@@ -2,22 +2,23 @@ import java.util.Scanner;
 
 public class NameGame {
 
-    public static void firstsong (String name) {
+    public static void verse (String name) {
 
-        System.out.println(name+" "+firstname+", bo-"+firstname);
-        System.out.println("Banana-fana"+firstname.substring(0,1)+"o-"+firstname);
+        System.out.println(name+" "+name+", bo-B"+name.substring(1));
+        System.out.println("Banana-fana fo-F"+name.substring(1));
+        System.out.println("Fee-fi-mo-M"+name.substring(1));
+        System.out.println(name.toUpperCase()+"!");
+        System.out.println("");
 
     }
     public static void main(String[]args){
 
-
         Scanner ng = new Scanner(System.in);
+        System.out.println("Enter first name");
+        System.out.println("Enter last name");
         String firstname = ng.nextLine();
         String lastname = ng.nextLine();
-
-
-
-
-
+        verse(firstname);
+        verse(lastname);
     }
 }
