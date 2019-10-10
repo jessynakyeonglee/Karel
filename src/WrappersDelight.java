5644adf0de6deffda6d37b1b9da39e9bf687f275
leena <leena@ssfs.org>
2019-10-10 15:09:07 -0400

    10/10/19 E block

--- src/WrappersDelight.java
+++ src/WrappersDelight.java
@@ -5,6 +5,17 @@ public class WrappersDelight {
         int num1 = 14;
         Integer num2 = new Integer(8);
         Double num3 = 2.5;
+        Double num4 = num2+num3;
+        num2-=3;
+        num3+=num1+num2+num4;
+        num1+=num2+num4;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+
+
+
 
 
     }
