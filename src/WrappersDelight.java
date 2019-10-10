64a783a650e61554c6edc050e8cf695ea56ec92b
leena <leena@ssfs.org>
2019-10-10 15:04:07 -0400

    10/10/19 E block

---
+++ src/WrappersDelight.java
@@ -0,0 +1,12 @@
+public class WrappersDelight {
+    public static void main(String[]args){
+
+
+        int num1 = 14;
+        Integer num2 = new Integer(8);
+        Double num3 = 2.5;
+
+
+    }
+
+}
