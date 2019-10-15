8afbe79a3cc976ee7692bbfea46047bf6162cb2f
leena <leena@ssfs.org>
2019-10-15 08:59:23 -0400

    10/15/19 E block

---
+++ src/Courses.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Courses {
+    public static void main(String[]args){
+
+        Scanner cs = new Scanner(System.in);
+        System.out.println("What is the description of the course?");
+
+
+
+    }
+}
